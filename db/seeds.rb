15dc2a9d0f6972a00a77b4e1264442d0f4bc4b6d
Dave Powers <djpowers89@gmail.com>
2014-02-20 14:55:03 -0500

    Create seed file to create or update queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@search_query = SearchQuery.find_or_initialize_by(terms: 'huggies diaper coupons')
+@result_group = ResultGroup.create(search_query: @search_query)
+@search_query.parse_ads.each do |ad|
+  returned_ad = Ad.find_or_create_by(headline: ad[0], url: ad[1], copy: ad[2])
+  @ad_result = AdResult.new(result_group: @result_group, ad: returned_ad)
+  @ad_result.save
+end
